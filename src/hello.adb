6a7560c3d25693d2302c0815acbb6d5738b8a4ae
Dinu Blanovschi <git@dnbln.dev>
2025-04-30 03:48:47 +0200

    fix: fix the insertion sort

--- src/hello.adb
+++ src/hello.adb
@@ -1,7 +1,12 @@
-with Libhello;
+with insort;
+with Ada.Text_IO;
 
 procedure Hello is
-
+   t: insort.Arr := (0, 10, 45, 1, -10, -100);
 begin
-   Libhello.Hello_World;
+   insort.sort(t);
+   for I in t'Range loop
+      Ada.Text_IO.Put_Line(Integer'Image(t(I)));
+   end loop;
+
 end Hello;
--- src/insort.ads
+++ src/insort.ads
@@ -1,10 +1,9 @@
 package insort with SPARK_Mode => On is
-   type Arr is array(Natural range <>) of Integer;
+   type Arr is array (Natural range <>) of Integer;
 
-   procedure sort(A: in out Arr)
+   procedure sort (A: in out Arr)
      with
-       Pre => A'Last > 0 and A'First < Integer'Last and A'Last < Integer'Last,
-     Post  => (for all I in A'First + 1 .. A'Last => A(I - 1) <= A(I));
-
+       Pre => A'Last > 0 and then A'First < Integer'Last and then A'Last < Integer'Last,
+     Post  => (for all I in A'First + 1 .. A'Last => A (I - 1) <= A(I));
 
 end insort;
